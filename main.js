bf8b5ae139b2666e0523874b55be9ed2e0604914
Uimarkku <Uimarkkugames@gmail.com>
2024-03-04 08:11:18 -0500

    Updated to a better system with css and js
    The website now features a blog and links

---
+++ main.js
@@ -0,0 +1,28 @@
+function ClickButton()
+{
+    window.location.href = "Page2.html"
+}
+function ClickItchButton()
+{
+    window.open("https://uimarkkugames.itch.io/")
+}
+function ClickYoutubeButton()
+{
+    window.open("https://youtube.com/@uimarkkugames/")
+}
+function ClickGithubButton()
+{
+    window.open("https://github.com/UimarkkuGames")
+}
+function ClickBackButton()
+{
+    window.location.href = "Index.html"
+}
+function ClickBlogButton()
+{
+    window.location.href = "Blog.html"
+}
+function ClickBlog1Button()
+{
+    window.location.href = "Blog-1.html"
+}
