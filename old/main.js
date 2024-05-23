958defac45789542520e5b3d657b526c27c3b92f
Uimarkku <Uimarkkugames@gmail.com>
2024-05-23 15:45:39 -0400

    Create main.js

---
+++ old/main.js
@@ -0,0 +1,54 @@
+function ClickButton()
+{
+    window.location.href = "Page2.html"
+}
+function ClickItchButton()
+{
+    window.open("https://uimarkkugames.itch.io/")
+}
+function ClickYoutubeButton()
+{
+    window.open("https://youtube.com/@uimarkkugames/")
+}
+function ClickGithubButton()
+{
+    window.open("https://github.com/UimarkkuGames")
+}
+function ClickBackButton()
+{
+    window.location.href = "index.html"
+}
+function ClickBlogButton()
+{
+    window.location.href = "blog.html"
+}
+function ClickBlog1Button()
+{
+    window.location.href = "blog-1.html"
+}
+function ClickBlog2Button()
+{
+    window.location.href = "blog-2.html"
+}
+function ClickDownloadButton()
+{
+    window.location.href = "downloadkodekillers.html"
+}
+function ClickReturnButton()
+{
+    var Kode = true;
+    window.location.href = "index.html"
+}
+function ClickDownloadZipButton()
+{
+window.open("https://github.com/UimarkkuGames/Uimarkkugames.github.io/releases/download/Kode_KIllers/Kode.Killers.zip")
+}
+function ClickDownloadInstallerButton()
+{
+    window.open("https://github.com/UimarkkuGames/Uimarkkugames.github.io/releases/download/Kode_KIllers/Kode.Killers.exe")
+}
+
+function ClickOtherButton()
+{
+    window.location.href = "downloads.html"
+}
